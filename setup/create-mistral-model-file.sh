7b377db04f0e473a52f063646ae91e90963e96ff
Brandon Hancock <brandon@brandonhancock.io>
2024-02-24 15:56:45 -0500

    Reorganize for turorial

---
+++ setup/create-mistral-model-file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables
+model_name="llama2"
+custom_model_name="crewai-llama2"
+
+# Get the base model
+ollama pull $model_name
+
+# Create the model file
+ollama create $custom_model_name -f ./Modelfile
